877d1e8a3a9cef3f0d81f90aeca7a773f5e02687
Juhosk <n0kaju00@students.oamk.fi>
2022-01-25 09:14:48 +0200

    Ensimmäinen versio ei toimivasti laskurista git remote add origin https://github.com/Juhosk/Kalorit.git git push origin -u origin master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [paino, setPaino] = useState(0);
+  const [Intensiteetti, setIntensiteetti] = useState(0);
+  const [sukupuoli, setSukupuoli] = useState("mies");
+  const [tulos, setTulos] = useState(0);
+
+
+  function laske(e) {
+    e.preventDefault();
+    let kalorit = 0;
+    if (sukupuoli == "mies") {
+      kalorit = (879 + 10.2 * paino) * Intensiteetti;
+    } else {
+      kalorit = (795 + 7,18 * paino) * Intensiteetti;
+    }
+    setTulos(kalorit);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <form>
+    <h3>Laske kalorikulutus</h3>
+    <div>
+      <label>Paino</label>
+      <input type="number" value={paino} onChange={e => setPaino(e.target.value)}/>
     </div>
+    <div>
+      <label>Intensiteetti</label>
+      <select onChange={e => setIntensiteetti(e.target.value)}>
+      <option value="1.3">Kevyt</option>
+      <option value="1.5">Tavallinen</option>
+      <option value="1.7">Kohtuullinen</option>
+      <option value="2">Kova</option>
+      <option value ="2.2">Erittäin kova</option>
+    </select>
+    </div>
+    <div>
+      <label>Sukupuoli</label>
+      <label><input type="radio" value="mies" defaultChecked onChange={e => setSukupuoli(e.target.value)}/>Mies</label>
+      <label><input type="radio" value="nainen" defaultChecked onChange={e => setSukupuoli(e.target.value)}/>Nainen</label>
+    </div>
+    <div>
+      <output>{tulos.ToFixed(0)}</output>
+    </div>
+    <button type="button" onClick={laske}>Calculate</button>
+    </form>
   );
 }
 
